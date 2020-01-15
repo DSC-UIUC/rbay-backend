e272590b4447c5de01f6eca2158dc3fda97de6a9
Thomas Yang <thomasy4@illinois.edu>
2020-01-15 16:33:15 -0500

    added creation of profile data

---
+++ public/main.js
@@ -0,0 +1,28 @@
+
+
+// var profilesElement = document.getElementById('profiles');
+// var profilesListener = null;
+
+document.getElementById("test2").innerHTML = "Hello JavaScript!";
+
+// function loadMessages() {
+// 	console.log("test");
+// 	profilesListener = firebase.firestore()
+// 		.collection('profiles')
+// 		.orderBy('name', 'desc')
+// 		.limit(4)
+// 		.onSnapshot((snaps) => {
+// 			profilesElement.innerHTML = "";
+// 			// Loop through documents in database
+// 			snaps.forEach((doc) => {
+// 			// Create an HTML entry fosr each document and add it to the chat
+// 			const entry = document.createElement("p");
+// 			entry.textContent = doc.data().name + ": " + doc.data().major;
+// 			profilesElement.appendChild(entry);
+// 		});
+// 	});
+// }
+
+// loadMessages();
+
+// console.log("test");
