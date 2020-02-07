e22bc8b6d1501b72a0c641fb207d82bb775b3bf0
Thomas Yang <thomasy4@illinois.edu>
2020-02-06 20:40:04 -0600

    creating GET cloud function for students and professors

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,65 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+var admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "https://research-bay.firebaseio.com"
+});
+
+var db = admin.firestore(); 
+
+
+exports.student = functions.https.onRequest((req, res) => {
+	var name = req.query.name;
+
+	switch(req.method) {
+		case 'GET':
+			if (name) {
+				var docRef = db.collection("students").doc(name);
+
+				docRef.get().then(docSnapshot => {
+					if (docSnapshot.exists) {
+						// Case where given name exists
+						var data = {};
+						data[name] = docSnapshot.data();
+						res.status(200).send(data);
+					} else {
+						res.status(404).send(name + ' does not exist!');
+					}
+				});
+			} else {
+				res.status(400).send("No query name given");
+			}
+			break;
+	}
+	return null;
+});
+
+
+exports.professors = functions.https.onRequest((req, res) => {
+
+	var name = req.query.name;
+
+	switch(req.method) {
+		case 'GET':
+			if (name) {
+				var docRef = db.collection("professors").doc(name);
+
+				docRef.get().then(docSnapshot => {
+					if (docSnapshot.exists) {
+						// Case where given name exists
+						var data = {};
+						data[name] = docSnapshot.data();
+						res.status(200).send(data);
+					} else {
+						res.status(404).send(name + ' does not exist!');
+					}
+				});
+			} else {
+				res.status(400).send("No query name given");
+			}
+			break;
+	}
+	return null;});
+
