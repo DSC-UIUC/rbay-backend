120f4489037230682602cdcfb4cdcaa65ba86595
Thomas Yang <thomasy4@illinois.edu>
2020-02-08 15:35:07 -0600

    adding basic functionality for CRUD on profiles

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,9 @@ admin.initializeApp({
   databaseURL: "https://research-bay.firebaseio.com"
 });
 
-var db = admin.firestore(); 
+var db = admin.firestore();
+const professor = "professors";
+const student 	= "students";
 
 function getProfile(standing, name, res) {
 	
@@ -18,7 +20,6 @@ function getProfile(standing, name, res) {
 			console.log("exists");
 			data[name] = docSnapshot.data();
 			res.status(200).send(data);
-			// console.log(data);
 		} else {
 			data["error"] = name + ' does not exist!';
 			res.status(404).send(data);
@@ -27,13 +28,59 @@ function getProfile(standing, name, res) {
 	return;
 }
 
+function createProfile(standing, name, res, payload) {
+	// TODO adding parameter checks
+	var docRef = db.collection(standing).doc(name);
+	docRef.set(payload);
+	res.status(200).send(name + " profile created");
+	return;
+}
+
+function deleteProfile(standing, name, res, payload) {
+	var docRef = db.collection(standing).doc(name);
+	docRef.get().then(docSnapshot => {
+		if (docSnapshot.exists) {
+			docRef.delete();
+			res.status(200).send(name + " deleted succesfully");
+		} else {
+			res.status(400).send(name + " does not exist");
+		}
+	});
+	return;
+}
+
+function updateProfile(standing, name, res, payload) {
+	// TODO adding parameter checks
+	var docRef = db.collection(standing).doc(name);
+	docRef.get().then(docSnapshot => {
+		if (docSnapshot.exists) {
+			docRef.update(payload);
+			res.status(200).send(name + " updated succesfully");
+		} else {
+			res.status(400).send(name + " does not exist");
+		}
+	})
+}
 
 exports.student = functions.https.onRequest((req, res) => {
 	var name = req.query.name;
 
 	switch(req.method) {
 		case 'GET':
-			getProfile("students", name, res);
+			if (name) {
+				getProfile(student, name, res);
+			} else {
+				res.status(400).send("Name not given");
+			}
+			break;
+		case 'POST':
+			createProfile(student, name, res, req.body);
+			break;
+		case 'DELETE':
+			deleteProfile(student, name, res);
+			break;
+		case 'PUT':
+			updateProfile(student, name, res, req.body);
 			break;
 	}
 	return null;
@@ -46,8 +93,22 @@ exports.professor = functions.https.onRequest((req, res) => {
 
 	switch(req.method) {
 		case 'GET':
-			getProfile("professors", name, res);
+			if (name) {
+				getProfile(professor, name, res);
+			} else {
+				res.status(400).send("Name not given");
+			}		
+			break;
+		case 'POST':
+			createProfile(professor, name, res, req.body);
+			break;
+		case 'DELETE':
+			deleteProfile(professor, name, res);
+			break;
+		case 'PUT':
+			updateProfile(professor, name, res, req.body);
 			break;
 	}
-	return null;});
+	return null;
+});
 
