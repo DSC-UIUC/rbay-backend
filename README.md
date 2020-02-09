e53ce87c0be036cb02294233a4774f287d5c388e
Thomas Yang <thomasy4@illinois.edu>
2020-02-09 17:33:14 -0600

    adding parameter checks to post/update endpoints and adding documentation

--- README.md
+++ README.md
@@ -15,9 +15,26 @@ Once deployed the base url will be https://us-central1-research-bay.cloudfunctio
 Gets the profile information of the given student using query string 'name'. Will return json format of the stored profile information if the given student exists.
 Ex. `/student?name=Bob Dylan`
 
+### POST /student
+Creates the profile information that is given through the http request. Use the query string 'name' to indicate the id of profile. Only parameters "GPA", "Year", "About Me", "Major", "Coursework", "Skills", and "Research Interests" will be added to the profile. Any other parameters will be ignored.
+
+### PUT /student
+Updates a profile given a query string 'name' and a json. Only the parameters "GPA", "Year", "About Me", "Major", "Coursework", "Skills", and "Research Interests" will be updated. Any other parameters will be ignored.
+
+### DELETE /student
+Deletes a profile given the query string 'name'.
 
 ## Professor Profiles
 
 ### GET /professor
 Gets the profile information of the given professor using query string 'name'. Will return json format of the stored profile information if the given professor exists.
 Ex. `/professor?name=David Hoffman`
+
+### POST /professor
+Creates the profile information that is given through the http request. Use the query string 'name' to indicate the id of profile. Only parameters "Bio", "Courses Taught", "Email", and "Research Areas" will be added to the profile. Any other parameters will be ignored.
+
+### PUT /professor
+Updates a profile given a query string 'name' and a json. Only the parameters "Bio", "Courses Taught", "Email", and "Research Areas" will be updated. Any other parameters will be ignored.
+
+### DELETE /professor
+Deletes a profile given the query string 'name'.
--- functions/index.js
+++ functions/index.js
@@ -11,13 +11,49 @@ var db = admin.firestore();
 const professor = "professors";
 const student 	= "students";
 
+// required fields for a profile
+const req_stud_fields = ["GPA", "Year", "About Me", "Major", "Coursework", "Skills", "Research Interests"];
+const req_prof_fields = ["Bio", "Courses Taught", "Email", "Research Areas"];
+
+/**
+ *	Removes any unknown elements in the given json
+ *
+ *	@param standing 	indicates whether profile is student or professor
+ *	@param json 		body of the http request
+ */
+function verifyJson(standing, json) {
+	var req_fields = [];
+
+	// checking if profile is student or professor
+	if (student.localeCompare(standing) == 0) {
+		req_fields = req_stud_fields;
+	} else if (professor.localeCompare(standing) == 0) {
+		req_fields = req_prof_fields;
+	}
+
+	var verifiedData = {};
+
+	for (var key in json) {
+		if (req_stud_fields.includes(key)) {
+			verifiedData[key] = json[key];
+		}
+	}
+	return verifiedData
+}
+
+/**
+ *
+ * @param standing 	indicates whether profile is student or professor
+ * @param name 		name of profile
+ * @param res 		response of request
+ */
 function getProfile(standing, name, res) {
 	
 	var docRef = db.collection(standing).doc(name);
 	var data = {};
+
 	docRef.get().then(docSnapshot => {
 		if (docSnapshot.exists) {
-			console.log("exists");
 			data[name] = docSnapshot.data();
 			res.status(200).send(data);
 		} else {
@@ -28,15 +64,35 @@ function getProfile(standing, name, res) {
 	return;
 }
 
+/**
+ * @param standing 	indicates whether profile is student or professor
+ * @param name 		name of profile
+ * @param res 		response of request
+ * @param payload 	request body
+ */
 function createProfile(standing, name, res, payload) {
-	// TODO adding parameter checks
+
+	var verifiedData = verifyJson(standing, payload);
+
 	var docRef = db.collection(standing).doc(name);
-	docRef.set(payload);
-	res.status(200).send(name + " profile created");
+
+	docRef.get().then(docSnapshot => {
+		if (!docSnapshot.exists) {
+			docRef.set(payload);
+			res.status(200).send(name + " profile created");
+		} else {
+			res.status(400).send(name + " already exists");
+		}
+	})
 	return;
 }
 
-function deleteProfile(standing, name, res, payload) {
+/**
+ * @param standing 	indicates whether profile is student or professor
+ * @param name 		name of profile
+ * @param res 		response of request
+ */
+function deleteProfile(standing, name, res) {
 	var docRef = db.collection(standing).doc(name);
 	docRef.get().then(docSnapshot => {
 		if (docSnapshot.exists) {
@@ -49,12 +105,18 @@ function deleteProfile(standing, name, res, payload) {
 	return;
 }
 
+/**
+ * @param standing 	indicates whether profile is student or professor
+ * @param name 		name of profile
+ * @param res 		response of request
+ * @param payload 	request body
+ */
 function updateProfile(standing, name, res, payload) {
-	// TODO adding parameter checks
+	var verifiedData = verifyJson(standing, payload);
 	var docRef = db.collection(standing).doc(name);
 	docRef.get().then(docSnapshot => {
 		if (docSnapshot.exists) {
-			docRef.update(payload);
+			docRef.update(verifiedData);
 			res.status(200).send(name + " updated succesfully");
 		} else {
 			res.status(400).send(name + " does not exist");
