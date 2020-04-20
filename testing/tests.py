8e4b3f15ad60795b5ae550930f4fd44c6ca3e4bb
Adi <asriram3@illinois.edu>
2020-04-20 23:39:43 +0800

    polished tests

--- testing/tests.py
+++ testing/tests.py
@@ -5,25 +5,27 @@ import json
 def signUp():
 	print("Signing up")
 
-	body = json.dumps({
+	body = {
 		"email": "test123@test.com",
 		"password": "test1234",
-		"is_student": True,
+		"is_student": "true",
 		"username": "pytest_one"
-	})
+	}
 
-	response = rq.post(
+	res = rq.post(
 		"https://us-central1-research-bay.cloudfunctions.net/signUp", data=body)
 
-	print(response.text)
+	print(res.json())
+	return res
 
 
-def signIn():
+def signIn(is_student):
 	print("Signing in")
 
 	url = "https://us-central1-research-bay.cloudfunctions.net/signIn"
 
-	body = {"email": "test123@test.com", "password": "test1234"}
+	body = {"email": "test123@test.com", "password": "test1234"} if is_student else {
+		"email": "proftest123@test.com", "password": "test1234"}
 
 	res = rq.post(url=url, data=body)
 
@@ -54,25 +56,26 @@ def getProfile(idToken):
 	return res
 
 
-def setProfile(idToken):
+def setProfile(idToken, profile):
 	print("Setting Profile")
 
 	# major and gpa don't seem to work
-	body = {
-		"idToken": idToken,
-		"aboutme": "I'm actually pretty awesome",
-		"gpa": 5,
-		"major": "awesome science",
-		"name": "Narcissist",
-		"year": 2,
-		"experience": "A lot",
-		"research interests": ["I", "Me", "Myself"],
-		"coursework": "Self studies"
-	}
+	# body = {
+	# 	"idToken": idToken,
+	# 	"aboutme": "I'm actually pretty awesome",
+	# 	"gpa": 5,
+	# 	"major": ["awesome science"],
+	# 	"name": "Narcissist",
+	# 	"year": 2,
+	# 	"experience": ["A lot"],
+	# 	"research_interests": ["I", "Me", "Myself"],
+	# 	"coursework": ["Self studies"],
+	# 	"skills":["reflection", "awareness"]
+	# }
 
 	url = "https://us-central1-research-bay.cloudfunctions.net/setProfile"
 
-	res = rq.post(url, data=body)
+	res = rq.post(url, data=profile)
 	print(res.text)
 
 
@@ -110,19 +113,23 @@ def createPosting(idToken):
 	print(res.text)
 	return res
 
+
 def deletePosting(idToken, postingId):
 	print("Deleting posting")
 
-	url = "https://us-central1-research-bay.cloudfunctions.net/deletePosting?idToken="+idToken+"&postingId="+postingId
+	url = "https://us-central1-research-bay.cloudfunctions.net/deletePosting?idToken=" + \
+		idToken+"&postingId="+postingId
 
 	res = rq.delete(url)
 	print(res.text)
 	return res
 
+
 def updatePosting(idToken, postingId):
 	print("Updating posting")
 
-	url = "https://us-central1-research-bay.cloudfunctions.net/updatePosting?idToken="+idToken+"&postingId="+postingId
+	url = "https://us-central1-research-bay.cloudfunctions.net/updatePosting?idToken=" + \
+		idToken+"&postingId="+postingId
 
 	body = {
 		"tags": ["fishing", "search"],
@@ -141,14 +148,17 @@ def updatePosting(idToken, postingId):
 	print(res.text)
 	return res
 
+
 def applyToPosting(idToken, postingId):
 	print("Applying to posting")
 
-	url = "https://us-central1-research-bay.cloudfunctions.net/applyToPosting?idToken="+idToken+"&postingId="+postingId
+	url = "https://us-central1-research-bay.cloudfunctions.net/applyToPosting?idToken=" + \
+		idToken+"&postingId="+postingId
 
 	res = rq.post(url)
 	print(res.text)
 	return res
+
 
 def main():
 	print("Starting Tests")
@@ -158,24 +168,57 @@ def main():
 	c = "string concat "+a
 	print(c)
 
-	res1 = signIn().json()
+	newAcc = 0
+	is_student = 1
 
+	if newAcc:
+		signUp()
+		return
+
+	res1 = signIn(is_student).json()
 	idToken = res1.get('data', {"idToken": None})['idToken']
 
 	testProfile = 0
 	testPostings = 1
 
-	if idToken and testPostings:
-		getUserPostings(idToken)
-		# createPosting(idToken, postingId)
-		# deletePosting(idToken, postingId)
-		# updatePosting(idToken, postingId)
-		# applyToPosting(idToken, postingId)
 	if idToken and testProfile:
-		# setProfile(idToken)
-		getProfile(idToken)
-		# deleteAcc(res1['data']['idToken'])
 
+		profile = {
+			"idToken": idToken,
+			"about_me": "I'm actually pretty awesome",
+			"gpa": 5.0,
+			"major": ["awesome science"],
+			"name": "Narcissist",
+			"year": 2,
+			"experience": ["A lot"],
+			"research_interests": ["I", "Me", "Myself"],
+			"coursework": ["Self studies"],
+			"skills": ["reflection", "awareness"]
+		}
+
+		setProfile(idToken, profile)
+
+		res = getProfile(idToken)
+		newProfile = res.json()["data"]
+
+		for key in profile:
+			if key == "idToken":
+				continue
+			if newProfile.get(key, "no key") != profile[key]:
+				print()
+				print(key, " has not been updated properly")
+				print("Passed in: ", profile[key])
+				print("On record: ", newProfile.get(key, "no key"))
+
+		# deleteAcc(res1['data']['idToken'])
+	if idToken and testPostings:
+		# res = createPosting(idToken)
+		# postingId = res.json()["data"]["id"]
+		postingId = "BbNwzzltaYK6rjsPTA2c"
+		# getUserPostings(idToken)
+		# deletePosting(idToken, postingId) # works
+		# updatePosting(idToken, postingId)
+		applyToPosting(idToken, postingId)
 
 
 # signIn()
